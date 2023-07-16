3b021137c4a800df029fd1ed2fa6fe9dc04a93f9
Mohamed Nour <71439813+MohamedNourSadek@users.noreply.github.com>
2023-07-16 18:53:13 +0300

    First Commit.

---
+++ Source/CityBuilding/CityBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CityBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITYBUILDING_API ACityBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
